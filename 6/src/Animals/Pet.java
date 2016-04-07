aab0debb0929d5523b718a6a4ee8f9272f2dc790
unknown <ies1541b@k3-426-7.edu.ugrasu>
2016-04-07 18:57:17 +0600

    Наследование

--- 6/src/Animals/Pet.java
+++ 6/src/Animals/Pet.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Animals;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  *
- * @author ies1541b
+ * @author ies1541b on 07.04.2016
+ * 
+ * 2. Написать три класса: Pet(домашнее животное) - с полями вес, пол, 
+ * возраст, Cat(кот) с полем имя и Dog(собака) с полем имя. Унаследовать 
+ * кота и собаку от животного. В классах кот и собака реализовать методы 
+ * вывода информации о животных.
  */
 public class Pet {
     
+    public double weight = 0;       // вес
+    String sex = null;              // пол
+    public double age = 0;          // возраст
+
+ //ввод 
+    public Pet() {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        System.out.println("Введите пол животного");
+            try {
+                sex = bufferedReader.readLine();
+            } catch (Exception e) {
+                System.out.println("Ошибка");
+            }
+                        
+        System.out.println("Введите вес");
+            try {
+                weight = Integer.parseInt(bufferedReader.readLine());
+            } catch (Exception e) {
+                System.out.println("Ошибка. Вес должен быть в цифрах");
+            }   
+            
+        System.out.println("Введите возраст");
+            try {
+                age = Integer.parseInt(bufferedReader.readLine());
+            } catch (Exception e) {
+                System.out.println("Ошибка. Возраст должен быть в цифрах");
+            }    
+    }
 }
